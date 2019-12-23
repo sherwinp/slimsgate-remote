0ac2479b8988d5970fc426339c8e89c5c8aff3cc
Sherwin.Price <Sherwin.Price@fda.hhs.gov>
2019-12-22 21:25:42 -0500

    add database connector

---
+++ install-driver.sh
@@ -0,0 +1,1 @@
+mvn install:install-file -Dfile=lib/sqljdbc42.jar -DgroupId=com.microsoft.sqlserver -DartifactId=sqljdbc42.jar -Dversion=4.0 -Dpackaging=jar
--- src/main/java/gov/fda/slimsgate/remote/Application.java
+++ src/main/java/gov/fda/slimsgate/remote/Application.java
@@ -2,6 +2,8 @@ package gov.fda.slimsgate.remote;
 
 import java.time.Clock;
 
+import javax.sql.DataSource;
+
 import org.apache.camel.CamelContext;
 import org.apache.camel.LoggingLevel;
 import org.apache.camel.builder.RouteBuilder;
@@ -11,9 +13,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Import;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.stereotype.Component;
 
 @SpringBootApplication
@@ -30,5 +36,5 @@ public class Application extends SpringBootServletInitializer {
 	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
 		return application.sources(applicationClass);
 	}
-	
+
 }
