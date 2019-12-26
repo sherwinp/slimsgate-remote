a9d4cd845455cf146e82d7e2fae63590234a75a1
Sherwin Price <sherwinprice@hotmail.com>
2019-12-25 23:25:27 -0500

    use custom queries in java.

--- src/main/java/gov/fda/slimsgate/remote/Application.java
+++ src/main/java/gov/fda/slimsgate/remote/Application.java
@@ -1,21 +1,21 @@
 package gov.fda.slimsgate.remote;
 
+import org.apache.camel.component.servlet.CamelHttpTransportServlet;
+import org.apache.camel.component.servlet.springboot.ServletMappingAutoConfiguration;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
+import org.springframework.boot.autoconfigure.web.servlet.WebMvcAutoConfiguration;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Import;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @SpringBootApplication
-@EnableWebMvc
-@EnableAutoConfiguration(
-exclude = {DataSourceAutoConfiguration.class, HibernateJpaAutoConfiguration.class})
-@ComponentScan(basePackages = "gov.fda.slimsgate")
-@Import({ WebConfig.class })
+@EnableAutoConfiguration(exclude = { HibernateJpaAutoConfiguration.class,
+		ServletMappingAutoConfiguration.class })
+@ComponentScan(basePackages = "gov.fda.slimgate.remote, gov.fda.slimsgate")
 public class Application extends SpringBootServletInitializer {
 	private static Class applicationClass = Application.class;
 
@@ -28,4 +28,11 @@ public class Application extends SpringBootServletInitializer {
 		return application.sources(applicationClass);
 	}
 
+	@Bean
+	ServletRegistrationBean<CamelHttpTransportServlet> servletRegistrationBean() {
+		ServletRegistrationBean<CamelHttpTransportServlet> servlet;
+		servlet = new ServletRegistrationBean<CamelHttpTransportServlet>(new CamelHttpTransportServlet(), "/run/*");
+		servlet.setName("CamelServlet");
+		return servlet;
+	}
 }
---
+++ src/main/java/gov/fda/slimsgate/remote/ContentRepository.java
@@ -0,0 +1,71 @@
+package gov.fda.slimsgate.remote;
+
+import java.util.ArrayList;
+
+import javax.persistence.EntityManagerFactory;
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+import org.springframework.stereotype.Component;
+
+@org.springframework.stereotype.Repository
+@EnableJpaRepositories(basePackages = "gov.fda.slimsgate.remote")
+public interface ContentRepository extends JpaRepository<Content, Long> {
+	
+	@Query(value="SELECT cntn FROM Content cntn WHERE cntn_id=?1 ORDER BY cntn_pk DESC")
+	ArrayList<Object[]> findContentByIdLimitedTo(String id, int limit);
+}
+
+@Component
+@PropertySource("classpath:application.properties")
+class Repository {
+    @Autowired
+    private Environment env;
+    
+	@Bean(name = "dataSource")
+	public DataSource dataSource() {
+		DriverManagerDataSource dataSource = new DriverManagerDataSource();
+		return dataSource;
+	}
+
+	@Bean(name="entityManagerFactory")
+	@ConditionalOnBean(name = "dataSource")
+	@ConditionalOnMissingBean
+	public LocalContainerEntityManagerFactoryBean entityManagerFactory() {
+		LocalContainerEntityManagerFactoryBean em = new LocalContainerEntityManagerFactoryBean();
+		em.setDataSource(dataSource());
+		em.setPackagesToScan("gov.fda.slimsgate.remote");
+		HibernateJpaVendorAdapter adapter = new HibernateJpaVendorAdapter();
+		if(env.getProperty("spring.jpa.show-sql")!=null) {
+			adapter.setShowSql(env.getProperty("spring.jpa.show-sql").startsWith("true"));
+		}
+		adapter.setDatabasePlatform(env.getProperty("spring.jpa.database-platform"));
+		em.setJpaVendorAdapter(adapter);
+		return em;
+	}
+
+	@Bean
+	@ConditionalOnMissingBean(type = "JpaTransactionManager")
+	JpaTransactionManager transactionManager(EntityManagerFactory entityManagerFactory) {
+		JpaTransactionManager transactionManager = new JpaTransactionManager();
+		transactionManager.setEntityManagerFactory(entityManagerFactory);
+		return transactionManager;
+	}
+}
